fa4f19f0edc3d9a490729d0e61c7e2b78bdeb484
bonafide9 <damian.bialek9@gmail.com>
2026-03-23 13:33:08 +0100

    P

--- Lab4/src/Main.java
+++ Lab4/src/Main.java
@@ -1,68 +1,30 @@
 import fuzzlib.FuzzySet;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
 
 public class Main {
 
-    static class Statystyki {
-        double suma = 0, sumaKwadratow = 0;
-        int liczbaElementow = 0;
-        double srednia, odchylenieStd;
-
-        void dodajWartosc(double val) {
-            suma += val;
-            sumaKwadratow += val * val;
-            liczbaElementow++;
-        }
-
-        void oblicz() {
-            srednia = suma / liczbaElementow;
-            double wariancja = (sumaKwadratow / liczbaElementow) - (srednia * srednia);
-            odchylenieStd = wariancja > 0 ? Math.sqrt(wariancja) : 0;
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         String[] nazwyKlas = {"Iris-setosa", "Iris-versicolor", "Iris-virginica"};
-
-        Statystyki[][] statystykiKlas = new Statystyki[3][4];
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 4; j++) statystykiKlas[i][j] = new Statystyki();
-        }
-
-        List<String> linie = Files.readAllLines(Paths.get("iris.data"));
-        for (String linia : linie) {
-            if (linia.isEmpty()) continue;
-            String[] czesci = linia.split(";");
-
-            int klasa = 0;
-            if (czesci[4].equals("Iris-versicolor")) klasa = 1;
-            else if (czesci[4].equals("Iris-virginica")) klasa = 2;
-
-            for (int i = 0; i < 4; i++) {
-                statystykiKlas[klasa][i].dodajWartosc(Double.parseDouble(czesci[i].replace(",", ".")));
-            }
-        }
-
         FuzzySet[][] model = new FuzzySet[3][4];
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 4; j++) {
-                statystykiKlas[i][j].oblicz();
+        // 1. SETOSA
+        model[0][0] = new FuzzySet(); model[0][0].newGaussian(5.006, 0.352);
+        model[0][1] = new FuzzySet(); model[0][1].newGaussian(3.428, 0.379);
+        model[0][2] = new FuzzySet(); model[0][2].newGaussian(1.462, 0.174);
+        model[0][3] = new FuzzySet(); model[0][3].newGaussian(0.246, 0.105);
 
-                // Tworzymy nowy zbiór rozmyty z biblioteki fuzzlib
-                FuzzySet fs = new FuzzySet();
+        // 2. VERSICOLOR
+        model[1][0] = new FuzzySet(); model[1][0].newGaussian(5.936, 0.516);
+        model[1][1] = new FuzzySet(); model[1][1].newGaussian(2.770, 0.314);
+        model[1][2] = new FuzzySet(); model[1][2].newGaussian(4.260, 0.470);
+        model[1][3] = new FuzzySet(); model[1][3].newGaussian(1.326, 0.198);
 
-                // fuzzlib posiada wbudowaną metodę do generowania dzwonu Gaussa
-                // Przyjmuje ona środek (średnią) oraz szerokość (odchylenie)
-                fs.newGaussian(statystykiKlas[i][j].srednia, statystykiKlas[i][j].odchylenieStd);
+        // 3. VIRGINICA
+        model[2][0] = new FuzzySet(); model[2][0].newGaussian(6.588, 0.636);
+        model[2][1] = new FuzzySet(); model[2][1].newGaussian(2.974, 0.322);
+        model[2][2] = new FuzzySet(); model[2][2].newGaussian(5.552, 0.552);
+        model[2][3] = new FuzzySet(); model[2][3].newGaussian(2.026, 0.275);
 
-                model[i][j] = fs;
-            }
-        }
-
-        System.out.println("Model rozmyty (oparty na fuzzlib) został zbudowany pomyślnie!\n");
+        System.out.println("Model rozmyty (dane statyczne) został zbudowany!\n");
 
         double[][] wektoryTestowe = {
                 {5.1, 3.5, 1.4, 0.2},
@@ -77,25 +39,21 @@ public class Main {
 
     private static void klasyfikujWektor(double[] wektor, String[] nazwyKlas, FuzzySet[][] model) {
         System.out.printf("Klasyfikacja wektora: [%.1f, %.1f, %.1f, %.1f]\n", wektor[0], wektor[1], wektor[2], wektor[3]);
-
         String najlepszaKlasa = "Nieznany";
         double najwyzszyWynik = -1.0;
 
         for (int i = 0; i < 3; i++) {
             double sumaPrzynaleznosci = 0.0;
-
             for (int j = 0; j < 4; j++) {
+
                 sumaPrzynaleznosci += model[i][j].getMembership(wektor[j]);
             }
-
-            System.out.printf(" - Wynik sumaryczny dla %s: %.4f\n", nazwyKlas[i], sumaPrzynaleznosci);
-
+            System.out.printf(" - Wynik dla %s: %.4f\n", nazwyKlas[i], sumaPrzynaleznosci);
             if (sumaPrzynaleznosci > najwyzszyWynik) {
                 najwyzszyWynik = sumaPrzynaleznosci;
                 najlepszaKlasa = nazwyKlas[i];
             }
         }
-
-        System.out.println("--> WERDYKT: Zwycięża " + najlepszaKlasa + "!\n");
+        System.out.println("--> WERDYKT: " + najlepszaKlasa + "!\n");
     }
 }
